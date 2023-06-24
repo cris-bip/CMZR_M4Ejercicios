cceed62fb383de83871cb270d0e0a77b623210ea
OITD <oitd@banxico.org.mx>
2023-06-24 13:02:27 -0600

    CZR. Agrega ejemplo de Spinner, utilizando ejemplos de paises como datos, y el adaptador de Android. Muestra el país seleccionado en un Toast.

--- app/src/main/java/com/cmzr/m4ejercicios/componentesgraficos/SelectionComponentActivity.kt
+++ app/src/main/java/com/cmzr/m4ejercicios/componentesgraficos/SelectionComponentActivity.kt
@@ -2,10 +2,9 @@ package com.cmzr.m4ejercicios.componentesgraficos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.CheckBox
-import android.widget.RadioGroup
-import android.widget.Toast
+import android.view.View
+import android.widget.*
+import android.widget.AdapterView.OnItemSelectedListener
 import com.cmzr.m4ejercicios.R
 
 class SelectionComponentActivity : AppCompatActivity() {
@@ -16,6 +15,34 @@ class SelectionComponentActivity : AppCompatActivity() {
 
         val btEnviar = findViewById<Button>(R.id.btEnviar)
         val hasCreditCard = findViewById<CheckBox>(R.id.hasTarjetaCredito)
+
+        val countrySelector = findViewById<Spinner>(R.id.countrySpinnerSelector)
+        val countries = arrayListOf<String>("México", "Argentina", "Estados Unidos", "Inglaterra",
+            "Japón", "Venezuela", "Rusia", "Colombia", "Alemania", "Korea del Norte", "Korea del Sur")
+        val countryAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, countries)
+        countryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        countrySelector.adapter = countryAdapter
+
+        countrySelector.onItemSelectedListener = object : OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val itemSelected = countries[position]
+
+                Toast.makeText(this@SelectionComponentActivity,
+                    "Selección del país: $itemSelected", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                Toast.makeText(this@SelectionComponentActivity,
+                     "Favor de seleccionar un país", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
 
         val radioGender = findViewById<RadioGroup>(R.id.radioGender)
 
