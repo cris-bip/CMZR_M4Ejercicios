30b838013aec06011eac8a46b2a5f672691ebb26
OITD <oitd@banxico.org.mx>
2023-06-23 19:25:12 -0600

    CZR. Agrega ejemplo de Frame Layout.

---
+++ app/src/main/java/com/cmzr/m4ejercicios/componentesgraficos/FrameActivity.kt
@@ -0,0 +1,12 @@
+package com.cmzr.m4ejercicios.componentesgraficos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cmzr.m4ejercicios.R
+
+class FrameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_frame)
+    }
+}
